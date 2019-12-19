9e98f6da5eac39723cd9a683bf2cef43bdd37f7f
Edwin Meek <emeekiv@gmail.com>
2019-12-19 19:36:27 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array){
+  for(let i=0; i<25; i++){
+      array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+   }
+   return array
+}
+
+
+
+function whileLoop(n) {
+
+        while ( n > 0 ) {
+
+    console.log(n)
+    n--
+    return n
+
+  }
+}
