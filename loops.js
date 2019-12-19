a993d55f66f81e5985e3c456d04084313f8bdcba
Edwin Meek <emeekiv@gmail.com>
2019-12-19 19:53:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,3 +13,13 @@ function whileLoop(n) {
   }
   return 'done'
 }
+
+var i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
